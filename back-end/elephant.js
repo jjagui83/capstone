42c8e0fefb417b64b12d70302c004c272bb628ef
pamela long <usmc.pnlong@gmail.com>
2021-11-10 12:27:06 -0500

    mini code update

--- back-end/elephant.js
+++ back-end/elephant.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const token =
-  "postgres://lftinrsd:8xWOylw4vIeJwOvr034JxmQwBD-V-g5A@fanny.db.elephantsql.com/lftinrsd";
+  "postgres://donffadg:R98AfsPlmzFCp0nfsayN3VLzkX_JxZf-@fanny.db.elephantsql.com/donffadg";
 const pg = require("pg");
 const creds = new pg.Client(token);
 
