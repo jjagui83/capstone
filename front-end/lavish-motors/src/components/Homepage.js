855b5d4fa7fec6e9fce3978c8f6c1f9b6647ff35
jjagui83 <joseaguilar0583@yahoo.com>
2021-11-10 14:05:28 -0600

    added test cars to homepage

--- front-end/lavish-motors/src/components/Homepage.js
+++ front-end/lavish-motors/src/components/Homepage.js
@@ -15,12 +15,12 @@ export default function Homepage() {
             </HomeContainer>
 
             <HomeContainer>
-            <img src="" alt="" />
-            <img src="" alt="" />
-            <img src="" alt="" />
-            <img src="" alt="" />
-            <img src="" alt="" />
-            <img src="" alt="" />
+            <img src="https://www.dupontregistry.com/autos/remote.jpg.ashx?404=default&width=644&height=428&urlb64=aHR0cDovL3ZlaGljbGUtcGhvdG9zLXB1Ymxpc2hlZC52YXV0by5jb20vZGYvN2YvZTAvOWEtZDEzZi00M2ZiLWJhZTctNjFhMjk2NmFkMTdjL2ltYWdlLTEuanBn&hmac=ArUsHMh02ZY" alt="" />
+            <img src="https://www.dupontregistry.com/autos/remote.jpg.ashx?404=default&width=644&height=428&urlb64=aHR0cDovL3ZlaGljbGUtcGhvdG9zLXB1Ymxpc2hlZC52YXV0by5jb20vZGYvN2YvZTAvOWEtZDEzZi00M2ZiLWJhZTctNjFhMjk2NmFkMTdjL2ltYWdlLTEuanBn&hmac=ArUsHMh02ZY" alt="" />
+            <img src="https://www.dupontregistry.com/autos/remote.jpg.ashx?404=default&width=644&height=428&urlb64=aHR0cDovL3ZlaGljbGUtcGhvdG9zLXB1Ymxpc2hlZC52YXV0by5jb20vZGYvN2YvZTAvOWEtZDEzZi00M2ZiLWJhZTctNjFhMjk2NmFkMTdjL2ltYWdlLTEuanBn&hmac=ArUsHMh02ZY" alt="" />
+            <img src="https://www.dupontregistry.com/autos/remote.jpg.ashx?404=default&width=644&height=428&urlb64=aHR0cDovL3ZlaGljbGUtcGhvdG9zLXB1Ymxpc2hlZC52YXV0by5jb20vZGYvN2YvZTAvOWEtZDEzZi00M2ZiLWJhZTctNjFhMjk2NmFkMTdjL2ltYWdlLTEuanBn&hmac=ArUsHMh02ZY" alt="" />
+            <img src="https://www.dupontregistry.com/autos/remote.jpg.ashx?404=default&width=644&height=428&urlb64=aHR0cDovL3ZlaGljbGUtcGhvdG9zLXB1Ymxpc2hlZC52YXV0by5jb20vZGYvN2YvZTAvOWEtZDEzZi00M2ZiLWJhZTctNjFhMjk2NmFkMTdjL2ltYWdlLTEuanBn&hmac=ArUsHMh02ZY" alt="" />
+            <img src="https://www.dupontregistry.com/autos/remote.jpg.ashx?404=default&width=644&height=428&urlb64=aHR0cDovL3ZlaGljbGUtcGhvdG9zLXB1Ymxpc2hlZC52YXV0by5jb20vZGYvN2YvZTAvOWEtZDEzZi00M2ZiLWJhZTctNjFhMjk2NmFkMTdjL2ltYWdlLTEuanBn&hmac=ArUsHMh02ZY" alt="" />
             </HomeContainer>
 
         </HomeContainer>
