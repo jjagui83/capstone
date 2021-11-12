9ade571fb92bd636d5dce4fba47230db6ce6c2fa
pamela long <usmc.pnlong@gmail.com>
2021-11-12 12:37:36 -0500

    code for supabase initial commit

--- back-end/elephant.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-const token =
-  "postgres://donffadg:R98AfsPlmzFCp0nfsayN3VLzkX_JxZf-@fanny.db.elephantsql.com/donffadg";
-const pg = require("pg");
-const creds = new pg.Client(token);
-
-module.exports = creds;
