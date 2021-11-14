f57b50ca3f65d1d134fc6fc014bc679ea682bc3c
xmcgriff92 <88303452+xmcgriff92@users.noreply.github.com>
2021-11-14 15:09:34 -0600

    grid gap

---
+++ front-end/lavish-motors/src/StyledComponents/VehicleStyle.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const VehicleImage = styled.img`
+height: 250px;
+width: 350px;
+`;
+
+export const VehicleContainer = styled.div`
+grid-gap: 10px 10px;
+`;
