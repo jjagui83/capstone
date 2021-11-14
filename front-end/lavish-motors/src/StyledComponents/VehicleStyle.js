b5b45f2a98217902cb31bbf28242894e1fa9b255
xmcgriff92 <88303452+xmcgriff92@users.noreply.github.com>
2021-11-14 15:55:02 -0600

    style

--- front-end/lavish-motors/src/StyledComponents/VehicleStyle.js
+++ front-end/lavish-motors/src/StyledComponents/VehicleStyle.js
@@ -3,8 +3,12 @@ import styled from "styled-components";
 export const VehicleImage = styled.img`
 height: 250px;
 width: 350px;
+border: 2px solid black;
+border-radius: 7px 7px 7px;
 `;
 
 export const VehicleContainer = styled.div`
-grid-gap: 10px 10px;
+grid-gap: 8px 8px;
+margin-bottom: 10px;
+background-color: white;
 `;
