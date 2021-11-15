9130744b00e8055ee1541c763fbb539bc5fd8d38
xmcgriff92 <88303452+xmcgriff92@users.noreply.github.com>
2021-11-15 10:52:44 -0600

    update banner

--- front-end/lavish-motors/src/StyledComponents/BannerStyle.js
+++ front-end/lavish-motors/src/StyledComponents/BannerStyle.js
@@ -6,36 +6,24 @@ width: 100%;
 `;
 
 export const BannerTextWrapper = styled.div`
-height: 80vh;
-background-image: url("https://images.unsplash.com/photo-1535448580089-c7f9490c78b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1021&q=80");
+height: 90vh;
+background-image: url("https://images.unsplash.com/photo-1570294645755-cbd74614549d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1499&q=80");
 -webkit-background-size: cover;
 background-position: fixed;
 background-repeat: no-repeat;
 `;
 
-export const BannerTextBanner = styled.div`
-width: 600px;
-height: 350px;
-background: inherit;
+export const BannerOverlay = styled.div`
 position: absolute;
-overflow: hidden;
-top: 50%;
-left: 50%;
-transform: translate(-50%, -50%)
-
-// &:before {
-//     // width: ;
-//     // height: ;
-//     // content: ;
-//     // position: absolute;
-//     // top: ;
-//     // left:;
-//     // bottom: 0;
-//     // right: 0;
-//     // background: inherit;
-//     // box-shadow: inset 0 0 0 300px rgba(255, 255, 255, 0.2);
-//     // filter: blur(10px);
-// }
+height: 90vh;
+width: 100%;
+left: 0;
+background: rgba(0, 0, 0, 0.5);
+color: white;
+display: flex;
+align-items: center;
+justify-content: center;
+flex-direction: column;
 `;
 
 export const BannerImageText = styled.div`
