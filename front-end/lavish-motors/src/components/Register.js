287973f08a2a7a49e0d9d790299fdac6c14c1e9c
pamela long <usmc.pnlong@gmail.com>
2021-11-15 10:54:39 -0500

    mini update

--- front-end/lavish-motors/src/components/Register.js
+++ front-end/lavish-motors/src/components/Register.js
@@ -1,100 +1,109 @@
-// import React from 'react';
-// import {useState} from 'react';
-// import { firstName, lastName, email } from "../actions/userActions"
-// import {useDispatch} from "react-redux";
-// import { LogInContainer } from '../StyledComponents/HomepageStyle';
-// import {createClient} from '@supabase/supabase-js'
-// import {useHistory} from "react-router"
+import React from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { LogInContainer } from "../StyledComponents/HomepageStyle";
+import { createClient } from "@supabase/supabase-js";
+import { useHistory } from "react-router";
 
-// const supabase = createClient("https://olnhrurwmonhkejpackd.supabase.co/", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjczMjY3NiwiZXhwIjoxOTUyMzA4Njc2fQ.Tm2RgeX9qDiT8vVT89NMvEj7MDvRzFrGjwPyfV6LGvI")
+const supabase = createClient(
+  "https://olnhrurwmonhkejpackd.supabase.co/",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjczMjY3NiwiZXhwIjoxOTUyMzA4Njc2fQ.Tm2RgeX9qDiT8vVT89NMvEj7MDvRzFrGjwPyfV6LGvI"
+);
 
-// function Register(props) {
-//     // const dispatch = useDispatch();
-//     const [formData,setFormData] = useState({firstName:"", lastName: "", email: "", password: ""});
-//     const history = useHistory();
+function Register(props) {
+  // const dispatch = useDispatch();
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+  const history = useHistory();
 
-//     const login = async (e)  => {
-//       e.preventDefault();
-//       const {user,session, error} = await supabase.auth.signIn({
-//         firstName: formData.firstName,
-//         lastName: formData.lastName,
-//         email: formData.email,
-//         password: formData.password,
-//       });
-//      if (session){
-//        history.push("/")
-//      }else {alert(error.message)};
-//     };
+  const login = async (e) => {
+    e.preventDefault();
+    const { user, session, error } = await supabase.auth.signIn({
+      firstName: formData.firstName,
+      lastName: formData.lastName,
+      email: formData.email,
+      password: formData.password,
+    });
+    if (session) {
+      history.push("/");
+    } else {
+      alert(error.message);
+    }
+  };
 
-//     const register = async (e) => {
-//       e.preventDefault();
-//       const {user,session, error} = await supabase.auth.signUp({
-//         email: formData.username,
-//         password: formData.password,
-//       });
-//       if (user) {
-//         history.push("/login");
-//       } else {
-//         alert(error.message);
-//       }
-//     };
+  const register = async (e) => {
+    e.preventDefault();
+    const { user, session, error } = await supabase.auth.signUp({
+      email: formData.username,
+      password: formData.password,
+    });
+    if (user) {
+      history.push("/login");
+    } else {
+      alert(error.message);
+    }
+  };
 
-//     return (
-//         <LogInContainer>
-//           <h1>Log In </h1>
-          
-         
-//                 <input   onChange={(e) =>
-//               setFormData({ ...formData, [e.target.name]: e.target.value })
-//             }
-//             type="text"
-//             placeholder="First Name"
-//             name="firstname"
-//             value={formData?.firstName}/>
+  return (
+    <LogInContainer>
+      <h1>Log In </h1>
 
-//             <input onChange={(e) =>
-//               setFormData({ ...formData, [e.target.name]: e.target.value })
-//             }
-//             type="text"
-//             placeholder="Last Name"
-//             name="lastname"
-//             value={formData?.lastName} />
+      <input
+        onChange={(e) =>
+          setFormData({ ...formData, [e.target.name]: e.target.value })
+        }
+        type="text"
+        placeholder="First Name"
+        name="firstname"
+        value={formData?.firstName}
+      />
 
-//             <input onChange={(e) =>
-//               setFormData({ ...formData, [e.target.name]: e.target.value })
-//             }
-//             type="text"
-//             placeholder="Email"
-//             name="email"
-//             value={formData?.email}/>
+      <input
+        onChange={(e) =>
+          setFormData({ ...formData, [e.target.name]: e.target.value })
+        }
+        type="text"
+        placeholder="Last Name"
+        name="lastname"
+        value={formData?.lastName}
+      />
 
-//           <input onChange={(e) =>
-//               setFormData({ ...formData, [e.target.name]: e.target.value })
-//             }
-//             type="text"
-//             placeholder="Password"
-//             name="password"
-//             value={formData?.password}/> 
-           
-//           {/* <input type="submit" /> */}
-//             {props?.register ? (
-//             <button
-//               onClick={(e) => register(e)}
-//               type="submit"
-//               value=""
-//             >
-//               Register
-//             </button>
-//           ) : (
-//             <button onClick={(e) => login(e)} type="submit" value="">
-//               Log In
-//             </button>
-//           )}
-//         </LogInContainer>
-//     )
-// }
+      <input
+        onChange={(e) =>
+          setFormData({ ...formData, [e.target.name]: e.target.value })
+        }
+        type="text"
+        placeholder="Email"
+        name="email"
+        value={formData?.email}
+      />
 
-// export default Register
+      <input
+        onChange={(e) =>
+          setFormData({ ...formData, [e.target.name]: e.target.value })
+        }
+        type="text"
+        placeholder="Password"
+        name="password"
+        value={formData?.password}
+      />
 
-      
-    
+      {/* <input type="submit" /> */}
+      {props?.register ? (
+        <button onClick={(e) => register(e)} type="submit" value="">
+          Register
+        </button>
+      ) : (
+        <button onClick={(e) => login(e)} type="submit" value="">
+          Log In
+        </button>
+      )}
+    </LogInContainer>
+  );
+}
+
+export default Register;
