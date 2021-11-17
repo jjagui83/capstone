964960a8fb99a0b5ea591399f542e0f4a33c5392
xmcgriff92 <88303452+xmcgriff92@users.noreply.github.com>
2021-11-16 19:14:07 -0600

    Update Testimonials.js

--- front-end/lavish-motors/src/components/Testimonials.js
+++ front-end/lavish-motors/src/components/Testimonials.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { TestimonialsContainer } from "../StyledComponents/TestimonialStyle";
+import { CardContainer, CardHeader, CardImage, CardTitle, CardPosition } from '../StyledComponents/TestimonialStyle';
 import { Container, Row, Col } from "react-bootstrap";
 import TestimonialCard from "./TestimonialCard";
 
@@ -8,7 +9,32 @@ export default function Testimonials() {
     <TestimonialsContainer>
       <Container>
         <Row>
-          <Col><TestimonialCard/></Col>
+          <Col>
+          <CardContainer>
+            <CardHeader>"blah blah blah"</CardHeader>
+            <CardImage src="https://www.glam.com/wp-content/uploads/2021/07/Screenshot-2021-07-09-6.32.47-PM.png" alt=""/>
+            <CardTitle>fhgfghfgfh</CardTitle>
+            <CardPosition>CEO</CardPosition>
+        </CardContainer>
+          </Col>
+
+          <Col>
+          <CardContainer>
+            <CardHeader>"blah blah blah"</CardHeader>
+            <CardImage src="https://www.glam.com/wp-content/uploads/2021/07/Screenshot-2021-07-09-6.32.47-PM.png" alt=""/>
+            <CardTitle>fhgfghfgfh</CardTitle>
+            <CardPosition>CEO</CardPosition>
+        </CardContainer>
+          </Col>
+
+            <Col>
+            <CardContainer>
+            <CardHeader>"blah blah blah"</CardHeader>
+            <CardImage src="https://www.glam.com/wp-content/uploads/2021/07/Screenshot-2021-07-09-6.32.47-PM.png" alt=""/>
+            <CardTitle>fhgfghfgfh</CardTitle>
+            <CardPosition>CEO</CardPosition>
+        </CardContainer>
+            </Col>
         </Row>
       </Container>
     </TestimonialsContainer>
