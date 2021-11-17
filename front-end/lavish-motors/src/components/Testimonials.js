6e76fab6e9fa6bec21f67cbb8eab85ef7c84263a
xmcgriff92 <88303452+xmcgriff92@users.noreply.github.com>
2021-11-16 19:18:31 -0600

    Update Testimonials.js

--- front-end/lavish-motors/src/components/Testimonials.js
+++ front-end/lavish-motors/src/components/Testimonials.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { TestimonialsContainer } from "../StyledComponents/TestimonialStyle";
-import { CardContainer, CardHeader, CardImage, CardTitle, CardPosition } from '../StyledComponents/TestimonialStyle';
+import {
+  CardContainer,
+  CardHeader,
+  CardImage,
+  CardTitle,
+  CardPosition,
+} from "../StyledComponents/TestimonialStyle";
 import { Container, Row, Col } from "react-bootstrap";
 import TestimonialCard from "./TestimonialCard";
 
@@ -10,31 +16,40 @@ export default function Testimonials() {
       <Container>
         <Row>
           <Col>
-          <CardContainer>
-            <CardHeader>"blah blah blah"</CardHeader>
-            <CardImage src="https://www.glam.com/wp-content/uploads/2021/07/Screenshot-2021-07-09-6.32.47-PM.png" alt=""/>
-            <CardTitle>fhgfghfgfh</CardTitle>
-            <CardPosition>CEO</CardPosition>
-        </CardContainer>
+            <CardContainer>
+              <CardHeader>"blah blah blah"</CardHeader>
+              <CardImage
+                src="https://www.glam.com/wp-content/uploads/2021/07/Screenshot-2021-07-09-6.32.47-PM.png"
+                alt=""
+              />
+              <CardTitle>fhgfghfgfh</CardTitle>
+              <CardPosition>CEO</CardPosition>
+            </CardContainer>
           </Col>
 
           <Col>
-          <CardContainer>
-            <CardHeader>"blah blah blah"</CardHeader>
-            <CardImage src="https://www.glam.com/wp-content/uploads/2021/07/Screenshot-2021-07-09-6.32.47-PM.png" alt=""/>
-            <CardTitle>fhgfghfgfh</CardTitle>
-            <CardPosition>CEO</CardPosition>
-        </CardContainer>
+            <CardContainer>
+              <CardHeader>"blah blah blah"</CardHeader>
+              <CardImage
+                src="https://d1bvpoagx8hqbg.cloudfront.net/259/e7f1793a4005be07cbd0f0e978cdb69a.jpg"
+                alt=""
+              />
+              <CardTitle>fhgfghfgfh</CardTitle>
+              <CardPosition>CEO</CardPosition>
+            </CardContainer>
           </Col>
 
-            <Col>
+          <Col>
             <CardContainer>
-            <CardHeader>"blah blah blah"</CardHeader>
-            <CardImage src="https://www.glam.com/wp-content/uploads/2021/07/Screenshot-2021-07-09-6.32.47-PM.png" alt=""/>
-            <CardTitle>fhgfghfgfh</CardTitle>
-            <CardPosition>CEO</CardPosition>
-        </CardContainer>
-            </Col>
+              <CardHeader>"blah blah blah"</CardHeader>
+              <CardImage
+                src="https://www.glam.com/wp-content/uploads/2021/07/Screenshot-2021-07-09-6.32.47-PM.png"
+                alt=""
+              />
+              <CardTitle>fhgfghfgfh</CardTitle>
+              <CardPosition>CEO</CardPosition>
+            </CardContainer>
+          </Col>
         </Row>
       </Container>
     </TestimonialsContainer>
